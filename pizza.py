0c869937067336597977a3010fdfdcf7bb2fb11f
RodrigoKaneki <rodrigo.ateu2018@gmail.com>
2017-08-17 18:50:16 -0300

    Update pizza.py

--- pizza.py
+++ pizza.py
@@ -1,3 +1,4 @@
+#importar no desafio 146
 def fazer_pizza(tamanho,*coberturas):
     """Resuma a pizza que estamos prestes a fazer."""
     print('\nO tamanho da pizza e: '+str(tamanho)+' com as seguintes coberturas ')
