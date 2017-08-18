f2dc818f4e3d1e5497344e8402adc009f9c60839
RodrigoKaneki <rodrigo.ateu2018@gmail.com>
2017-08-18 20:04:50 -0300

    ^_^

--- desafio133.py
+++ desafio133.py
@@ -3,3 +3,4 @@ def descri_animal(tipo_animal, nome_animal):
     print(tipo_animal+' e um animal lindo, seu nome e '+nome_animal.title())
 descri_animal('cachorro','tedy')
 descri_animal(nome_animal='tedy',tipo_animal='cachorro')
+
