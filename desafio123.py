9172d4df4f1aac68ee1adbf019d0ed38244075d2
RodrigoKaneki <rodrigo.ateu2018@gmail.com>
2017-08-18 19:58:37 -0300

    ^_^

--- desafio123.py
+++ desafio123.py
@@ -1,7 +1,9 @@
-# Definimos loop while True, a variável cidade recebe a mensagem de m1 pedindo para
-#o usuario digitar os lugares que ela visitou, se o que ele digitar for igual a sair break , ele para
-# e e o loop acaba, senão sera executado o bloco else, que faz uma concatenção
-# com a frase que o usuario digitou...
+"""
+Definimos loop while True, a variável cidade recebe a mensagem de m1 pedindo para
+usuario digitar os lugares que ela visitou, se o que ele digitar for igual a sair break , ele para
+e e o loop acaba, senão sera executado o bloco else, que faz uma concatenção
+com a frase que o usuario digitou...
+"""
 m1 = 'Digite os lugares que vc visitou '
 m1 += 'digite "sair" acabar. '
 while True:
@@ -10,6 +12,4 @@ while True:
         break
     else:
         print('Eu adoraria ir para '+cidade+' !')
-
-
 
