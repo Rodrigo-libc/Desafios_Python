a2503c452bce3db2ecae9bd18f1e2f224bf71612
RodrigoKaneki <rodrigo.ateu2018@gmail.com>
2017-08-18 19:56:50 -0300

    ^_^

--- desafio122.py
+++ desafio122.py
@@ -1,8 +1,11 @@
-# criamos uma variável com valor True para o loop while
-# colocamos um input() para que o usuário digite algo
-# se o que o usuário digitar for == a "sair" a variável se torna false 
-# e o loop acaba, se for qualquer outra coisa então então sera executado o bloco
-#else e sera exibida a oque ele digitou ..
+""" 
+criamos uma variável com valor True para o loop while
+colocamos um input() para que o usuário digite algo
+se o que o usuário digitar for == a "sair" a variável se torna false 
+e o loop acaba, se for qualquer outra coisa então então sera executado o bloco
+else e sera exibida a oque ele digitou ..
+
+"""
 m1 = '!loop em processo: '
 ativo = True
 while ativo:
