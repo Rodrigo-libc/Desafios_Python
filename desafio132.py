20bdf3274e214c5f937073d85c6353c46eb5bf16
RodrigoKaneki <rodrigo.ateu2018@gmail.com>
2017-08-18 20:04:27 -0300

    ^_^

--- desafio132.py
+++ desafio132.py
@@ -3,3 +3,4 @@ def descri_animal(tipo_animal, nome_animal):
     print(tipo_animal+' e muito lindo, o outro de chama '+nome_animal.title())
 descri_animal('boby','tedy')
 descri_animal('lilo','lupy')
+
